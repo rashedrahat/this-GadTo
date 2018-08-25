4f3dc1750b7da526a7ca161e3fe4b8285d97386f
Rashed Ahmed Rahat <37572137+rashedrahat@users.noreply.github.com>
2018-08-25 22:47:49 +0600

    Header & Footer design
    Implemented only on index.php

---
+++ header.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+* {box-sizing: border-box;}
+
+body { 
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.header {
+  overflow: hidden;
+  background-color: #f1f1f1;
+  padding: 20px 10px;
+}
+
+.header a {
+  float: left;
+  color: black;
+  text-align: center;
+  padding: 12px;
+  text-decoration: none;
+  font-size: 18px; 
+  line-height: 25px;
+  border-radius: 4px;
+}
+
+.header a.logo {
+  font-size: 25px;
+  font-weight: bold;
+}
+
+.header a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.header a.active {
+  background-color: dodgerblue;
+  color: white;
+}
+
+.header-right {
+  float: right;
+}
+
+input[type=text] {
+    width: 130px;
+    box-sizing: border-box;
+    border: 2px solid #ccc;
+    border-radius: 4px;
+    font-size: 16px;
+    background-color: white;
+    background-image: url('searchicon.png');
+    background-position: 10px 10px; 
+    background-repeat: no-repeat;
+    padding: 12px 20px 12px 40px;
+    -webkit-transition: width 0.4s ease-in-out;
+    transition: width 0.4s ease-in-out;
+}
+
+input[type=text]:focus {
+    width: 99%;
+}
+
+@media screen and (max-width: 500px) {
+  .header a {
+    float: none;
+    display: block;
+    text-align: left;
+  }
+  .header-right {
+    float: none;
+  }
+}
+</style>
+</head>
+<body>
+
+<div class="header">
+  <a href="#default" class="logo">GadTo</a>
+
+  <span style="float: right;">
+    <form>
+      <input type="text" name="search" placeholder="Search..">
+    </form>
+  </span>
+  <span class="header-right">
+    <a class="active" href="home.php">Home</a>
+    <a href="signIn.php">Sign In</a>
+    <a href="signUp.php">Sign Up</a>
+    <a href="#about">About Us</a>
+    <a href="#FAQ">FAQ</a>
+    <a href="#help">Help</a>
+    <a href="#feedback">Feedback</a>
+  </span>
+</div>
+</body>
+</html>
--- index.php
+++ index.php
@@ -2,123 +2,94 @@
 <head>
     <title>Website Home Page</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 </head>
 
 <body>
-    <div style="padding:10; text-align:right;">
-        <!-- Load icon library -->
-                            &emsp;&emsp;&emsp;&emsp; &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp; &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
-                            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <?php
+        include 'header.php';
+    ?>
+    <div style="padding-left:20px;">
+        <div style="padding-top: 15px;">
+            <table align="center" border="1" width="1000" height="300">
+                <tr>
+                    <th align="center" colspan="3">
+                        <h2><i>GadTo (Gadget Tottho) is different. Here is how:</i></h2>
+                    </th>
+                </tr>
 
-                            <!-- The form -->
-                            <form class="example" action="">
-                                <input type="text" placeholder="Search" name="search" onkeyup="searchedResult(this.value)">
-                                <button type="submit">
-                                    <i class="fa fa-search"></i>
-                                </button>
-        <a href="index.php"><img style="float: left; padding-left: 10" src="GadTo.jpg" width="80" height="50"></a>
-        <a href="home.php">Home</a> | <a href=#about>About Us</a> | <a href="SignIn.php">Sign In</a> | <a href="SignUp.php">Sign Up</a> | <a href=#FAQ>FAQ</a> | <a href=#help>Help</a> | <a href=#feedback>Feedback</a>
-    </div>
-    <div style="text-align: center;">
-        <i id="sug"><small style="text-align: center;">&emsp;&emsp;&emsp;&emsp; &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<b>Quick See:</b> Write something in "Search" to see here the seraching thing is or not in website?</small></i>
-        <script>
-            function searchedResult(str)
-            {
-                if (str.length == 0) {document.getElementById("sug").innerHTML = "Empty!";}
+                <tr>
+                    <td align="center">
+                        <img src="gs.jpg" alt="Specifications icon" width="50" height="60">
+                        <h3>Gadget Specification</h3><br>
+                        Each gadget is shown with lots of specifications to give best overview about a gadget for the passionte gadget lover.
+                    </td>
+                    <td align="center">
+                        <img src="tk.png" alt="Price icon" width="50" height="60">
+                        <h3>
+                            Gadget Price
+                        </h3><br>
+                        Height, Lowest & average. No price is missing. All in one place for you to choose the best desirable price among all the prices.
+                    </td>
                 
-                else{
-                    var xhttp = new XMLHttpRequest();
-                    xhttp.onreadystatechange = function()
-                    {
-                        if(this.readyState == 4 && this.status == 200)
-                        {document.getElementById("sug").innerHTML = this.responseText;}
-                    };
-                    xhttp.open("GET", "searchedResData.php?q="+str,true);
-                    xhttp.send();
-                }
-            }
-  </script>
+                    <td align="center">
+                        <img src="rr.jpg" alt="Review icon" width="50" height="60">
+                        <h3>
+                            Gadget & E-commerce Company reviews
+                        </h3><br>
+                        Share us feelings by giving a review. Earn trust by helping people through your reviews. Right review to choose between what to do & what not to do.
+                    </td>
+                </tr>       
+            </table>
+        </div>
+        <div style="padding-top: 20px;">
+            <table align="center" border="1" width="1000" height="200">
+                <tr>
+                    <th align="center" colspan="9">Categories</th>
+                </tr>   
+                <tr align="center">
+                    <td><a href="Page2.php">Computer</td>
+                    <td><a href=#mobile>Mobile</td>
+                    <td><a href=#watch>Watch</td>
+                    <td><a href=#c4>C4</td>
+                    <td><a href=#c5>C5</td>
+                    <td><a href=#c6>C6</td>
+                    <td><a href=#c7>C7</td>
+                    <td><a href=#c8>C8</td>
+                    <td><a href=#c9>C9</td>
+                </tr>    
+                
+                <tr align="center">
+                    <td><a href=#c5>C10</td>
+                    <td><a href=#c5>C11</td>
+                    <td><a href=#c5>C12</td>
+                    <td><a href=#c5>C13</td>
+                    <td><a href=#c5>C14</td>
+                    <td><a href=#c5>C15</td>
+                    <td><a href=#c5>C16</td>
+                    <td><a href=#c5>C17</td>
+                    <td><a href=#c5>C18</td>
+                </tr>
+                
+                <tr align="center">
+                    <td><a href=#c5>C19</td>
+                    <td><a href=#c5>C20</td>
+                    <td><a href=#c5>C21</td>
+                    <td><a href=#c5>C22</td>
+                    <td><a href=#c5>C23</td>
+                    <td><a href=#c5>C24</td>
+                    <td><a href=#c5>C25</td>
+                    <td><a href=#c5>C26</td>
+                    <td><a href=#c5>C27</td>
+                </tr>
+            </table>
+        </div>
+        <div style="padding:5; text-align:center;">
+            <a href="#next">Next</a> > <a href="#2">2</a> > .. > <a href="#2">5</a>
+        </div>
     </div>
-    <div style="padding-top: 80">
-        <table align="center" border="1" width="1000" height="300">
-            <tr>
-                <th align="center" colspan="3">
-                    <h2><i>GadTo (Gadget Tottho) is different. Here is how:</i></h2>
-                </th>
-            </tr>
-
-            <tr>
-                <td align="center">
-                    <img src="gs.jpg" alt="Specifications icon" width="50" height="60">
-                    <h3>Gadget Specification</h3><br>
-                    Each gadget is shown with lots of specifications to give best overview about a gadget for the passionte gadget lover.
-                </td>
-                <td align="center">
-                    <img src="tk.png" alt="Price icon" width="50" height="60">
-                    <h3>
-                        Gadget Price
-                    </h3><br>
-                    Height, Lowest & average. No price is missing. All in one place for you to choose the best desirable price among all the prices.
-                </td>
-            
-                <td align="center">
-                    <img src="rr.jpg" alt="Review icon" width="50" height="60">
-                    <h3>
-                        Gadget & E-commerce Company reviews
-                    </h3><br>
-                    Share us feelings by giving a review. Earn trust by helping people through your reviews. Right review to choose between what to do & what not to do.
-                </td>
-            </tr>       
-        </table>
-    </div>
-    <div style="padding-top: 20">
-        <table align="center" border="2" width="1000" height="200">
-            <tr>
-                <th align="center" colspan="9">
-                Categories
-            </th>
-            </tr>   
-            <tr align="center">
-                <td><a href="Page2.php">Computer</td>
-                <td><a href=#mobile>Mobile</td>
-                <td><a href=#watch>Watch</td>
-                <td><a href=#c4>C4</td>
-                <td><a href=#c5>C5</td>
-                <td><a href=#c6>C6</td>
-                <td><a href=#c7>C7</td>
-                <td><a href=#c8>C8</td>
-                <td><a href=#c9>C9</td>
-            </tr>    
-            
-            <tr align="center">
-                <td><a href=#c5>C10</td>
-                <td><a href=#c5>C11</td>
-                <td><a href=#c5>C12</td>
-                <td><a href=#c5>C13</td>
-                <td><a href=#c5>C14</td>
-                <td><a href=#c5>C15</td>
-                <td><a href=#c5>C16</td>
-                <td><a href=#c5>C17</td>
-                <td><a href=#c5>C18</td>
-            </tr>
-            
-            <tr align="center">
-                <td><a href=#c5>C19</td>
-                <td><a href=#c5>C20</td>
-                <td><a href=#c5>C21</td>
-                <td><a href=#c5>C22</td>
-                <td><a href=#c5>C23</td>
-                <td><a href=#c5>C24</td>
-                <td><a href=#c5>C25</td>
-                <td><a href=#c5>C26</td>
-                <td><a href=#c5>C27</td>
-            </tr>
-        </table>
-    </div>
-    <div style="padding:5; text-align:center;">
-        <a href="#next">Next</a> > <a href="#2">2</a> > .. > <a href="#2">5</a>
-    </div>
+    <?php
+        include 'footer.php';
+    ?>
 </body>
 </html>
