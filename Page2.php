94fa62191941b4c5a01e68081aae034e8328b7a9
Mehedi Hasan Shuvo <hasanshuvom884@gmail.com>
2018-09-19 14:52:52 +0600

    next button

--- Page2.php
+++ Page2.php
@@ -41,8 +41,24 @@ if ($category_name=="") {
                     </a>
                   </td>
                 </tr>
-
               <?php } ?>
+              <tr>
+                <td>
+                  <div >
+                    <nav aria-label="Page navigation ">
+                      <ul class="pagination justify-content-center">
+                        <li class="page-item ">
+                          <a class="page-link" href="#" tabindex="-1">Previous</a>
+                        </li>
+                        <li class="page-item"><a class="page-link" href="#">1</a></li>
+                        <li class="page-item">
+                          <a class="page-link" href="#">Next</a>
+                        </li>
+                      </ul>
+                    </nav>
+                  </div>
+                </td>
+              </tr>
 
         </table>
         </div>
